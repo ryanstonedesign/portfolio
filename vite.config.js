f90f4bb348f883d06940b08114f1741df7742484
Ryan Stone <rstone50.psu@gmail.com>
2026-01-12 08:58:47 -0500

    Fix React Router basename for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   resolve: {
     alias: {
       '@': '/src',
